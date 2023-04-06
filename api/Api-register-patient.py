e1ebfe2699f062f3da23cb7c757e5e7de933d03e
sebtick <90834370+sebtick@users.noreply.github.com>
2023-04-06 23:10:00 +0200

    push avec le patient en dur

--- api/Api-register-patient.py
+++ api/Api-register-patient.py
@@ -1,66 +1,94 @@
 from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///patients.db'
-db = SQLAlchemy(app)
 
+patients = [
+    {
+        '_id_patient': 1,
+        '_name': 'John Doe',
+        '_gender': 'Male',
+        '_arrival_date': '2022-03-14 10:30:00',
+        '_priority': 1,
+        '_state': 'waiting'
+    },
+    {
+        '_id_patient': 2,
+        '_name': 'Jane Doe',
+        '_gender': 'Female',
+        '_arrival_date': '2022-03-15 08:45:00',
+        '_priority': 2,
+        '_state': 'waiting'
+    }
+]
 
-class Patient(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
-    gender = db.Column(db.String(10))
-    condition = db.Column(db.String(100))
-    priority = db.Column(db.Integer)
-    status = db.Column(db.String(20), default='waiting')
+@app.route('/get-patient/<int:patient_id>', methods=['GET'])
+def get_patient(patient_id):
+    patient = next((p for p in patients if p['_id_patient'] == patient_id), None)
+    if not patient:
+        return jsonify({'message': 'Patient not found'}), 404
     
+    return jsonify(patient), 200
 
-@app.route('/register-patient', methods=['POST'])
-def register_patient():
+
+@app.route('/add-patient', methods=['POST'])
+def add_patient():
     patient_data = request.get_json()
-    patient = Patient(name=patient_data['name'], gender=patient_data['gender'],
-                      condition=patient_data['condition'], priority=patient_data['priority'])
-    db.session.add(patient)
-    db.session.commit()
-    return 'Patient registered successfully'
+    patient = {
+        '_id_patient': len(patients) + 1,
+        '_name': patient_data['_name'],
+        '_gender': patient_data['_gender'],
+        '_arrival_date': patient_data['_arrival_date'],
+        '_priority': patient_data['_priority'],
+        '_state': 'waiting'
+    }
+    patients.append(patient)
+    return jsonify({
+        '_id_patient': patient['_id_patient'],
+        '_name': patient['_name'],
+        '_gender': patient['_gender'],
+        '_arrival_date': patient['_arrival_date'],
+        '_priority': patient['_priority'],
+        '_state': patient['_state']
+    }), 201
+    
+@app.route('/get-most-urgent-patient', methods=['GET'])
+def get_most_urgent_patient():
+    most_urgent_patient = None
+    for patient in patients:
+        if patient['_state'] == 'waiting':
+            if most_urgent_patient is None or patient['_priority'] > most_urgent_patient['_priority']:
+                most_urgent_patient = patient
 
-
-@app.route('/get-next-patient', methods=['GET'])
-def get_next_patient():
-    next_patient = Patient.query.filter_by(status='waiting').order_by(Patient.priority.desc()).first()
-    if not next_patient:
+    if most_urgent_patient is None:
         return jsonify({'message': 'No patient found in the waiting list'}), 404
     
-    next_patient.status = 'in consultation'
-    db.session.commit()
+    most_urgent_patient['_state'] = 'in consultation'
     
     return jsonify({
-        'id': next_patient.id,
-        'name': next_patient.name,
-        'gender': next_patient.gender,
-        'condition': next_patient.condition,
-        'priority': next_patient.priority
+        '_id_patient': most_urgent_patient['_id_patient'],
+        '_name': most_urgent_patient['_name'],
+        '_gender': most_urgent_patient['_gender'],
+        '_arrival_date': most_urgent_patient['_arrival_date'],
+        '_priority': most_urgent_patient['_priority']
     }), 200
-    
-@app.route('/refer-patient', methods=['POST'])
-def refer_patient():
-    patient_data = request.get_json()
-    patient_id = patient_data['id']
-    new_specialist = patient_data['specialist']
-    patient = Patient.query.get(patient_id)
-    if not patient:
-        return jsonify({'message': 'Patient not found'}), 404
-    patient.specialist = new_specialist
-    db.session.commit()
-    return jsonify({'message': 'Patient referred successfully'}), 200
 
-@app.route('/finish-patient-cycle', methods=['POST'])
-def finish_patient_cycle():
-    patient_data = request.get_json()
-    patient_id = patient_data['id']
-    patient = Patient.query.get(patient_id)
+@app.route('/update-patient/<int:patient_id>', methods=['PUT'])
+def update_patient(patient_id):
+    patient = next((p for p in patients if p['_id_patient'] == patient_id), None)
     if not patient:
         return jsonify({'message': 'Patient not found'}), 404
-    patient.status = 'finished'
-    db.session.commit()
-    return jsonify({'message': 'Patient cycle finished successfully'}), 200
+
+    data = request.get_json()
+    patient['_state'] = data.get('_state', patient['_state'])
+    patient['_priority'] = data.get('_priority', patient['_priority'])
+
+    return jsonify(patient), 200
+    
+@app.route('/delete-patient/<int:id>', methods=['DELETE'])
+def delete_patient(id):
+    for patient in patients:
+        if patient['_id_patient'] == id:
+            patients.remove(patient)
+            return jsonify({'message': f'Patient with ID {id} has been deleted'}), 200
+    
+    return jsonify({'message': 'Patient not found'}), 404
