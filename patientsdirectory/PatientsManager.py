1e8646f7d058e17c53b7818dfe6dc84fad55a4ac
JeanPhilippeCaetano <92150404+JeanPhilippeCaetano@users.noreply.github.com>
2023-04-06 23:22:08 +0200

    add @classmethod

--- patientsdirectory/PatientsManager.py
+++ patientsdirectory/PatientsManager.py
@@ -2,12 +2,13 @@ from copy import copy
 
 
 class PatientsManager:
-    def __init__(self):
-        self._patients = []
+    _patients = []
 
+    @classmethod
     def create_patient(self, patient):
         self._patients.append(patient)
 
+    @classmethod
     def read_patient(self, attribute_type=None, attribute_value=None):
         patients_copy = self._patients.copy()
         patients_sorted = sorted(patients_copy, key=(lambda b: b._priority), reverse=True)
@@ -15,9 +16,11 @@ class PatientsManager:
             return [patient for patient in patients_sorted if getattr(patient, attribute_type) == attribute_value]
         return patients_sorted
 
+    @classmethod
     def update_patient(self, patient_id, attribute_type=None, attribute_value=None):
         if attribute_type is not None and attribute_value is not None:
             setattr(self._patients[patient_id], attribute_type, attribute_value)
 
+    @classmethod
     def delete_patient(self, patient_id):
         self._patients.pop(patient_id)
